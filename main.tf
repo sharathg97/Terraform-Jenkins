e22c5360d9287ba24031e277feab0ab5ed0eacdd
sharathg97 <42089276+sharathg97@users.noreply.github.com>
2026-01-23 11:17:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
-resource "local_file" "file1"
-{
-filename=var.fname
-content=var.fcontent
+resource "local_file" "file1" {
+    filename = var.fname
+    content = var.fcontent
 }
-variable "fname"{
-default="abc.txt"
-type=string
+variable "fname" {
+    default  = "abc.txt"
+    type = string
 }
-variable "fcontent"{
-default="Good day,this is the content inside abc file"
-type=string
+variable "fcontent" {
+    default  = "Good day,this is the content inside abc file"
+    type = string
 }
-
