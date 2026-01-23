c87082858f828558405ede01af57f97999d20cd6
sharathg97 <42089276+sharathg97@users.noreply.github.com>
2026-01-23 12:49:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,17 @@ variable "fcontent" {
     default  = "Good day,this is the content inside abc file"
     type = string
 }
+resource "azurerm_resource_group" "rg" {
+  location = var.location
+  name     = var.rg_name
+}
+
+variable location{
+	default = "westus"
+	type = string
+}
+variable rg_name{
+	default = "sharrgpip"
+	type = string
+}
+
