b04c4d40d161e2b1b42e3a0e30afe99c021e24ab
sharathg97 <42089276+sharathg97@users.noreply.github.com>
2026-01-28 23:04:42 +0530

    Update res.tf

--- res.tf
+++ res.tf
@@ -76,7 +76,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("/home/azureuser/.ssh/id_rsa.pub")
+    public_key = file("/var/lib/jenkins/id_rsa.pub")
   }
 
   os_disk {
