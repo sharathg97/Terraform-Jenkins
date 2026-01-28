e22dced6fcf7592efcd56a700454c810cc86b744
sharathg97 <42089276+sharathg97@users.noreply.github.com>
2026-01-28 13:05:16 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "resource_group_location" {
+  description = "Location of the Azure Resource Group"
+  value       = azurerm_resource_group.rg.location
+}
+
+output "resource_group_id" {
+  description = "ID of the Azure Resource Group"
+  value       = azurerm_resource_group.rg.id
+}
