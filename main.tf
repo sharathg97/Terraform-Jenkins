8376e6cbd991b839855bf088032637f0a932c857
sharathg97 <42089276+sharathg97@users.noreply.github.com>
2026-01-28 13:04:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,14 @@
-resource "local_file" "file1" {
-    filename = var.fname
-    content = var.fcontent
-}
-variable "fname" {
-    default  = "abc.txt"
-    type = string
-}
-variable "fcontent" {
-    default  = "Good day,this is the content inside abc file"
-    type = string
-}
 resource "azurerm_resource_group" "rg" {
-  location = var.location
-  name     = var.rg_name
+    location = var.location
+    name = var.rg_name
 }
 
-variable location{
-	default = "westus"
-	type = string
+variable location {
+    default = "westus"
+    type = string
 }
-variable rg_name{
-	default = "sharrgpip"
-	type = string
+variable rg_name {
+    default = "sharrgpjen"
+    type = string
 }
 
